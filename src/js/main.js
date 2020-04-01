122075638d965fa52eb1ff8b9a3837707993beca
codedev.it <elle@codedev.it>
2020-04-01 22:57:43 +0200

    final result

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
 import '@fortawesome/fontawesome-free/css/all.css'
 import '../scss/main.scss';
 import {myFunct} from './testFunct';
+
 $(()=>{
     myFunct('.emph','white');
+    function copyToClipboard(text) {
+        var text = $('.link-repo').text()
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(text).select();
+        document.execCommand("copy");
+        $temp.remove();
+        alert('text copied!')
+    }
+    $('.copy-repo').on('click', copyToClipboard)
 }
 )
--- src/js/testFunct.js
+++ src/js/testFunct.js
@@ -4,4 +4,5 @@ const myFunct = (colorWord,color) =>{
     console.log(theWord)
     theWord.css('color',color)
 }
+
 export {myFunct}
